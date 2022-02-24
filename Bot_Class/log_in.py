0c9c97bcf6cfda65e8dee083bf23208d2ac1695a
Lucio Massimi <48844501+pulpoma@users.noreply.github.com>
2022-02-24 10:16:18 -0300

    Update log_in.py

--- Bot_Class/log_in.py
+++ Bot_Class/log_in.py
@@ -7,6 +7,7 @@ class LogIn():
 
     @property
     def initialize(self):
+        """Crea un Log in por cuenta, lo pense asi para que sea mas comoda la organizacion de las cuentas en las que se van a correr los bot"""
         import pyRofex as pyRofex
 
         pyRofex.initialize(user = self.user, password = self.password, account = self.account, environment = pyRofex.Environment.REMARKET)
